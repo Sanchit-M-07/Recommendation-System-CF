fdc6b81d380c52a0c85557ed7b388b5ee331c82d
Sanchit-M-07 <sanchitmishra2005@gmail.com>
2025-12-26 00:06:51 +0530

    Remove extra blank line in recommender class

--- collaborative_filtering_recommender.py
+++ collaborative_filtering_recommender.py
@@ -3,7 +3,6 @@ import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 from scipy.sparse.linalg import svds
 
-
 class CollaborativeFilteringRecommender:
     """
     Collaborative filtering recommender system.
